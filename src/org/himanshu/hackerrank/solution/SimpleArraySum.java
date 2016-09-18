551a0cccd81f2c423abaddffa91d60a56a6ab4bb
Himanshu Gusain <Himanshu Gusain@192.168.1.5>
2016-09-18 20:05:24 +0530

    Formatting and documentation for the file

--- src/org/himanshu/hackerrank/solution/SimpleArraySum.java
+++ src/org/himanshu/hackerrank/solution/SimpleArraySum.java
@@ -10,26 +10,33 @@ import java.util.Scanner;
  *
  */
 public class SimpleArraySum {
-	
+
 	public static void main(String[] args) {
-        /* Enter your code here. Read input from STDIN. Print output to STDOUT. Your class should be named Solution. */
-        
-        int sum = 0;
-        Scanner in = new Scanner(System.in);
-        int size = in.nextInt();
-        int arr[] = new int[size];
-        for(int index = 0; index < size ;index++){
-        	arr[index] = in.nextInt();
-        }
-        sum = findSumOfElements(arr);
-        System.out.println(sum);
-    }
-    private static int findSumOfElements(int array[]){
-        int sum=0;
-        for(int index = 0; index<array.length; index++){
-            sum += array[index];
-        }
-        return sum;
-    }
-	
+		int sum;
+		Scanner in = new Scanner(System.in);
+		int size = in.nextInt();
+		int arr[] = new int[size];
+		for (int index = 0; index < size; index++) {
+			arr[index] = in.nextInt();
+		}
+		sum = findSumOfElements(arr);
+		System.out.println(sum);
+	}
+
+	/**
+	 * This will find the sum of the array
+	 * 
+	 * @param array
+	 *            the array on which the operation will take place
+	 * @return return the sum of the elements of the array
+	 */
+	private static int findSumOfElements(int array[]) {
+		int sum = 0;
+		for (int index = 0; index < array.length; index++) {
+			sum += array[index];
+		}
+		return sum;
+
+	}
+
 }
