308d439c790a85ab3c033b93969640ac85e56ab7
Himanshu Gusain <Himanshu Gusain@192.168.1.10>
2017-08-06 21:21:31 +0530

    Adding Class super reduced string

---
+++ src/org/himanshu/hackerrank/strings/solution/SuperReducedString.java
@@ -0,0 +1,28 @@
+/**
+ * 
+ */
+package org.himanshu.hackerrank.strings.solution;
+
+import java.util.Scanner;
+
+/**
+ * @author Himanshu Gusain
+ *
+ */
+public class SuperReducedString {
+	
+	static String super_reduced_string(String s){
+		return s;
+        // Complete this function
+    }
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String s = in.next();
+        String result = super_reduced_string(s);
+        System.out.println(result);}
+
+}
