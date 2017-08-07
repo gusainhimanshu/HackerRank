5019fa9acd3414450d12fa1b8b0e5b798e24bb34
Himanshu Gusain <Himanshu Gusain@192.168.1.10>
2017-08-07 12:28:55 +0530

    updating the package names

---
+++ src/org/himanshu/hackerrank/algo/warmup/solution/DiagonalDifference.java
@@ -0,0 +1,42 @@
+package org.himanshu.hackerrank.algo.warmup.solution;
+
+import java.util.Scanner;
+
+/**
+ * @author Himanshu Gusain
+ *
+ */
+public class DiagonalDifference {
+
+	public static void main(String args[]) {
+		try (Scanner input = new Scanner(System.in);) {
+			int size = input.nextInt();
+			int array[][] = new int[size][size];
+			for (int i = 0; i < size; i++) {
+				for (int j = 0; j < size; j++) {
+					array[i][j] = input.nextInt();
+				}
+			}
+			result(array, size);
+		}
+	}
+
+	public static void result(int array[][], int size) {
+		int primaryDiagonalSum = 0;
+		int secondaryDiagonalSum = 0;
+		int difference;
+		for (int i = 0; i < size; i++) {
+			for (int j = 0; j < size; j++) {
+				if (i == j) {
+					primaryDiagonalSum += array[i][j];
+				}
+				if ((size - 1) == (i + j)) {
+					secondaryDiagonalSum += array[i][j];
+				}
+			}
+		}
+		difference = Math.abs(primaryDiagonalSum - secondaryDiagonalSum);
+		System.out.println(difference);
+
+	}
+}
