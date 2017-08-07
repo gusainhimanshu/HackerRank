54dae65db781ad87d9f7f8b1a41c2005a73dca81
Himanshu Gusain <Himanshu Gusain@192.168.1.10>
2017-08-07 12:55:43 +0530

    formatting

--- src/org/hg/hr/java/oop/solution/JavaAbstract.java
+++ src/org/hg/hr/java/oop/solution/JavaAbstract.java
@@ -2,31 +2,35 @@ package org.hg.hr.java.oop.solution;
 
 import java.util.Scanner;
 
-abstract class Book{
+abstract class Book {
 	String title;
+
 	abstract void setTitle(String s);
-	String getTitle(){
+
+	String getTitle() {
 		return title;
 	}
-	
+
 }
-class MyBook extends Book{
-    
-    public void setTitle(String s){
-        title = s;
-    }
+
+class MyBook extends Book {
+
+	public void setTitle(String s) {
+		title = s;
+	}
 }
 
 public class JavaAbstract {
 
 	public static void main(String[] args) {
-		//Book new_novel=new Book(); This line prHMain.java:25: error: Book is abstract; cannot be instantiated
-		Scanner sc=new Scanner(System.in);
-		String title=sc.nextLine();
-		MyBook new_novel=new MyBook();
+		// Book new_novel=new Book(); This line prHMain.java:25: error: Book is
+		// abstract; cannot be instantiated
+		Scanner sc = new Scanner(System.in);
+		String title = sc.nextLine();
+		MyBook new_novel = new MyBook();
 		new_novel.setTitle(title);
-		System.out.println("The title is: "+new_novel.getTitle());
-      	sc.close();
-		}
+		System.out.println("The title is: " + new_novel.getTitle());
+		sc.close();
+	}
 
 }
