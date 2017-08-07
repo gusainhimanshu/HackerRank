cfdf4be3149358c04e91a7e93bf20f6a5bdb29a4
Himanshu Gusain <Himanshu Gusain@192.168.1.10>
2017-08-07 12:16:56 +0530

    Adding the solution and leaked connections closed

--- src/org/himanshu/hackerrank/strings/solution/SuperReducedString.java
+++ src/org/himanshu/hackerrank/strings/solution/SuperReducedString.java
@@ -1,6 +1,3 @@
-/**
- * 
- */
 package org.himanshu.hackerrank.strings.solution;
 
 import java.util.Scanner;
@@ -10,19 +7,45 @@ import java.util.Scanner;
  *
  */
 public class SuperReducedString {
-	
-	static String super_reduced_string(String s){
-		return s;
-        // Complete this function
-    }
+
+	static String super_reduced_string(String s) {
+		int index = 0;
+		while (index < (s.length() - 1)) {
+			if (s.charAt(index) == s.charAt(index + 1)) {
+				s = String.join("", s.substring(0, index), s.substring(index + 2, s.length()));
+				index = 0;
+			} else {
+				index++;
+			}
+		}
+		if (s.isEmpty()) {
+			return "Empty String";
+		} else {
+			return s;
+		}
+
+	}
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
-        String s = in.next();
-        String result = super_reduced_string(s);
-        System.out.println(result);}
+		Scanner in = null;
+		try {
+			in = new Scanner(System.in);
+			String s = in.next();
+			String result = super_reduced_string(s);
+			System.out.println(result);
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+
+			e.printStackTrace();
+		} finally {
+			if (in != null) {
+				in.close();
+			}
+
+		}
+	}
 
 }
